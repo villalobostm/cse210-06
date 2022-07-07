0b88821c49ab66ba0bea71258dff7ce1d1469e06
Joseph <per16068@byui.edu>
2022-07-06 20:35:51 -0400

    Added limits to the racket

--- pong/game/scripting/move_racket_action.py
+++ pong/game/scripting/move_racket_action.py
@@ -2,12 +2,12 @@ from constants import *
 from game.casting.point import Point
 from game.scripting.action import Action
 
-
+"This class controls the the racket and the limits of the racket"
 class MoveRacketAction(Action):
 
     def __init__(self, player):
         self._player = player
-
+    "Controls the limits of the racket"
     def execute(self, cast, script, callback):
         racket = cast.get_first_actor(self._player)
         body = racket.get_body()
